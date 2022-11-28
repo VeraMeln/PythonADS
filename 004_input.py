c58ab420f189d86162ef64a8ea94cba9d39b72c7
VeraMeln <85120624+VeraMeln@users.noreply.github.com>
2022-11-28 15:33:04 +0300

    Add files via upload

---
+++ 004_input.py
@@ -0,0 +1,41 @@
+# I Пример использования input()
+name = input('Введите свое имя: ')
+print('Вас зовут ', name)
+
+# # II пример преобразования типов переменных
+# # поместим числа в переменные как строки и посмотрим их тип
+# first_number = '23'
+# print('Переменная first_number имеет тип: ', type(first_number))
+
+# second_number = '0.25'
+# print('Переменная second_number имеет тип: ', type(second_number))
+
+# # Сложим first_number и second_number без преобразования типов
+# print('first_number + second_number =', first_number + second_number)
+# print()
+# print('Теперь найдем сумму first_number и second_number после преобразования:')
+
+# # преобразуем число из first_number к типу int
+# first_number = int(first_number)
+# print('Переменная first_number имеет тип: ', type(first_number))
+
+# # преобразуем число из second_number к типу float
+# second_number = float(second_number) 
+# print('Переменная second_number имеет тип: ', type(second_number))
+
+# print('first_number + second_number =', first_number + second_number)
+
+# Задачи
+# 1.	Пользователь вводит с клавиатуры два числа по очереди. 
+# Выведите на экран их сумму.
+
+# 2.	Пользователь вводит число. 
+# Выведите таблицу умножения до 10 на это число.
+
+# 3.	Пользователь вводит с клавиатуры количество минут, 
+# которые он тратит на компьютерные игры в день. Программа должна 
+# вывести на экран количество часов в неделю.
+
+# 4.	Напишите программу, которая умеет решать уравнения вида a + x = b. 
+# Пользователь вводит с клавиатуры числа а  и b, 
+# а программа выводит, чему равен x
